caf57203769fccd48e9d9fc49b6ece22cf6ac267
JONATHAN <oguguo.peter@gmail.com>
2023-06-27 14:53:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+
+
+function App() {
+  return (
+    <main className="App">
+     
+      <Nav/>
+      <Footer/>
+
+    </main>
+  );
+}
+
+export default App;
